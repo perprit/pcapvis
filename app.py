51d76553a2d747eab2e117dda5b9d5f0fcd783d7
dandelin <dandelin@snu.ac.kr>
2015-05-14 14:11:23 +0900

    flask upload

---
+++ app.py
@@ -0,0 +1,42 @@
+import os
+from flask import Flask, request, redirect, url_for, send_from_directory
+from werkzeug import secure_filename
+
+UPLOAD_FOLDER = 'uploaded_pcap'
+ALLOWED_EXTENSIONS = set(['pcap'])
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route('/')
+def hello():
+    return 'hello world'
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_pcap():
+    if request.method == 'POST':
+        fp = request.files['file']
+        if fp and allowed_file(fp.filename):
+            filename = secure_filename(fp.filename)
+            fp.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file', filename=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form action="" method=post enctype=multipart/form-data>
+        <p>
+            <input type=file name=file>
+            <input type=submit value=Upload>
+    </form>
+    '''
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=12321)
