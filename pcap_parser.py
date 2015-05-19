fee339ce2b36256eecc77bb434a7b156329e448a
Wonjae Kim <fkdlvm1109@gmail.com>
2015-05-19 19:28:04 +0900

    update pcap_parser
    {
    'ts' : ts,
    'src' : socket.inet_ntoa(ip_hdr.src),
    'sport' : ip_hdr.data.sport,
    'dst' : socket.inet_ntoa(ip_hdr.dst),
    'sport' : ip_hdr.data.sport,
    'ipv' : ip_hdr.v,
    'seq' : ip_hdr.data.seq,
    'ack' : ip_hdr.data.ack,
    'win' : ip_hdr.data.win,
    'flags' : {
    'SYN': True if ip_hdr.data.flags & dpkt.tcp.TH_SYN else False,
    'ACK': True if ip_hdr.data.flags & dpkt.tcp.TH_ACK else False,
    'RST': True if ip_hdr.data.flags & dpkt.tcp.TH_RST else False,
    'PUSH': True if ip_hdr.data.flags & dpkt.tcp.TH_PUSH else False,
    'FIN': True if ip_hdr.data.flags & dpkt.tcp.TH_FIN else False
    }
    }

--- pcap_parser.py
+++ pcap_parser.py
@@ -1,7 +1,35 @@
-import dpkt, json
+import dpkt, json, socket
 
 def pcap_to_json(fp):
+
     pcap = dpkt.pcap.Reader(fp)
-    eth = lambda x: dpkt.ethernet.Ethernet(x)
-    tcps = [{'ts': ts, 'dport': eth(buf).data.data.dport, 'sport': eth(buf).data.data.sport} for ts, buf in pcap if eth(buf).data.p == 6]
+
+    tcps = [
+    	{
+    		'ts' : ts,
+    		'src' : socket.inet_ntoa(ip_hdr.src),
+    		'sport' : ip_hdr.data.sport,
+    		'dst' : socket.inet_ntoa(ip_hdr.dst),
+    		'sport' : ip_hdr.data.sport,
+    		'ipv' : ip_hdr.v,
+    		'seq' : ip_hdr.data.seq,
+    		'ack' : ip_hdr.data.ack,
+    		'win' : ip_hdr.data.win,
+    		'flags' : {
+    					'SYN': True if ip_hdr.data.flags & dpkt.tcp.TH_SYN else False,
+    					'ACK': True if ip_hdr.data.flags & dpkt.tcp.TH_ACK else False,
+    					'RST': True if ip_hdr.data.flags & dpkt.tcp.TH_RST else False,
+    					'PUSH': True if ip_hdr.data.flags & dpkt.tcp.TH_PUSH else False,
+    					'FIN': True if ip_hdr.data.flags & dpkt.tcp.TH_FIN else False
+    				}
+    	}
+    	for ts, ip_hdr
+    	in [(ts, dpkt.ethernet.Ethernet(buf).data) for ts, buf in list(pcap)]
+    	if ip_hdr.p == 6
+    ]
+
     return json.dumps(tcps)
+
+if __name__ == '__main__':
+	with open('../github.pcap', 'rb') as fp:
+		print pcap_to_json(fp)
